31661767d54c3b7f3b9ecf8f6f4b95da5e716d49
DuarteB <eduduarte.dev@gmail.com>
2020-07-10 14:54:23 -0300

    Adicionando js basico e funções iniciais, e icones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+function consultaCep() {
+    const cep = document.getElementById('CEP').value;
+    const res = document.getElementById('Resultado');
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(response) {
+            const { logradouro, complemento, bairro, localidade, uf, cep} = response;
+            $(res).html(`${logradouro} ${complemento} - ${bairro}. ${localidade} - ${uf} .Cep: ${cep}`);
+        },
+    });
+}
